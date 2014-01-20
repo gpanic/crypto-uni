328b8a643d4a45a69baa2a4bfcd55a44768667c4
Gregor Panic <gregor.panic@gmail.com>
2014-01-20 14:37:50 +0100

    asdf

--- secmail/secmail.py
+++ secmail/secmail.py
@@ -1,19 +1,25 @@
 from M2Crypto import BIO, Rand, SMIME
 import smtplib
+from email.mime.text import MIMEText
 
 def mkbuff(text):
 	return BIO.MemoryBuffer(text)
 
-buff = mkbuff("we send this text")
+
+msg = "we send this text"
+msg_mime = MIMEText(msg)
+
+buff = mkbuff(msg)
 Rand.load_file('randpool.dat', -1)
 s = SMIME.SMIME()
-s.load_key('signer_private.pem', 'signer_public.pem')
+s.load_key('sender-private.pem', 'sender-public.pem')
 p7 = s.sign(buff)
 
 out = BIO.MemoryBuffer();
 out.write('From: %s\r\n' % "vision810@gmail.com")
 out.write('To: %s\r\n' % "gregor.panic@gmail.com")
-out.write('Subject: %s\r\n'% "test mail") 
+out.write('Subject: %s\r\n'% "test mail")
+out.write(msg_mime.as_string())
 s.write(out, p7)
 out.close()
 
